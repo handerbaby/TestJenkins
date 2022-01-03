aa8b618bda06174a6d66dfb47e00a57e2ec9a35a
duqihui <duqihui@uino.com>
2022-01-03 23:48:32 +0800

    vue

---
+++ edutrainvue/src/components/Video.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Video',
+  data () {
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+<style scoped>
+.test{
+    color: black;
+}
+</style>
---
+++ edutrainvue/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Video from '@/components/Video'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Video',
+      name: 'Video',
+      component: Video
+    }
+  ]
+})
